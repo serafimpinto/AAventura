3f240744088ef042d582f00bf21baabbd4fcce41
Serafim Pinto <smcp92@gmail.com>
2014-05-20 19:22:27 +0100

    Registar

--- AAventura/AAventura/Models/Utilizador.cs
+++ AAventura/AAventura/Models/Utilizador.cs
@@ -74,6 +74,10 @@ namespace AAventura.Models
         [Required]
         [Display(Name = "User name")]
         public string UserName { get; set; }
+        
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
